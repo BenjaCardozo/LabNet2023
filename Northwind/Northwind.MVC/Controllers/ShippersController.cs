e6064891187408d88c4e0621eb7c3cb80f50eba4
BenjaCardozo <benjamin.cardozo0@gmail.com>
2023-03-22 19:41:22 -0300

    empezando tp

---
+++ Northwind/Northwind.MVC/Controllers/ShippersController.cs
@@ -0,0 +1,90 @@
+﻿using Northwind.Data;
+using Northwind.Logic.Application;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Northwind.MVC.Controllers
+{
+    public class ShippersController : Controller
+    {
+        // GET: Shippers
+        public ActionResult Index()
+        {
+            var logic = new ShippersLogic();
+            List<Shippers> shippers = logic.GetAll();
+            return View(shippers);
+        }
+
+        // GET: Shippers/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Shippers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Shippers/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Shippers/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Shippers/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Shippers/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Shippers/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
