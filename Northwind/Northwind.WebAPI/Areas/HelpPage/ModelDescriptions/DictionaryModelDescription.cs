8e782de98e922d4470d632894580a4b28186ec42
BenjaCardozo <benjamin.cardozo0@gmail.com>
2023-04-03 08:05:43 -0300

    Limpiando back-end y terminando proyecto.

--- Northwind/Northwind.WebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Northwind.WebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Northwind/Northwind.WebAPI/Global.asax.cs
+++ Northwind/Northwind.WebAPI/Global.asax.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
 
 namespace Northwind.WebAPI
 {
@@ -15,8 +11,6 @@ namespace Northwind.WebAPI
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
 
     }
